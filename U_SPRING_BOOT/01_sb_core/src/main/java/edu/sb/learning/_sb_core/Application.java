c7ab420afbf8ad1c042a94225ac303f02a36dc2f
unknown <munna.cse.pust@gmail.com>
2023-12-05 01:17:22 +0600

    start spring boot

---
+++ U_SPRING_BOOT/01_sb_core/src/main/java/edu/sb/learning/_sb_core/Application.java
@@ -0,0 +1,13 @@
+package edu.sb.learning._sb_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ U_SPRING_BOOT/01_sb_core/src/test/java/edu/sb/learning/_sb_core/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sb.learning._sb_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
